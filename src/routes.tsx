9d7b5a7b5668e87fc8b98c0390995b75a8e64663
Joas-Assuncao <joassalvior@gmail.com>
2023-10-25 08:07:09 -0300

    improve: redirect to dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import { Dashboard } from "./pages/dashboard";
 
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
