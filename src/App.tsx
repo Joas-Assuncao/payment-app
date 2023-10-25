c888f265f55a5fdbadef88832f5a484654ff021c
Joas-Assuncao <joassalvior@gmail.com>
2023-10-25 17:57:23 -0300

    feat: add spinner, header, sidebar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Router } from "./routes";
 import { queryClient } from "./services/queryClient";
 
@@ -7,6 +9,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <Header />
+        <Sidebar />
         <Router />
       </BrowserRouter>
     </QueryClientProvider>
