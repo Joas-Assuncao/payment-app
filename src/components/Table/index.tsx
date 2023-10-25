e4402a7579be483bf1e60d970b6de59b3d9a71f6
Joas-Assuncao <joassalvior@gmail.com>
2023-10-25 18:00:12 -0300

    improve: table component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,37 +1,91 @@
-export function Table({ columnsName, dataSource }: TableProps) {
-  const data: [string, string][][] = dataSource.map((dataItem) => {
-    return Object.entries(dataItem);
-  });
-
+export function Table({ columnsName, dataSource, totalCount }: TableProps) {
   return (
-    <table className="w-full text-sm text-left text-gray-500">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-        <tr>
-          {columnsName.map((columnName) => (
-            <th scope="col" className="px-6 py-3" key={columnName}>
-              {columnName}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((dataItem, index) => {
-          return (
-            <tr className="bg-white border-b" key={index}>
-              {dataItem.map(([, value], index) => (
-                <td className="px-6 py-4" key={index}>
-                  {value}
-                </td>
-              ))}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <table className="text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            {columnsName?.map((columnName) => (
+              <th scope="col" className="px-6 py-3" key={columnName}>
+                {columnName}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dataSource?.map((dataItem, index) => {
+            return (
+              <tr className="bg-white border-b" key={index}>
+                {dataItem.map(([, value], index) => (
+                  <td className="px-6 py-4" key={index}>
+                    {value}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {totalCount && totalCount > 10 && (
+        <>
+          <div className="flex flex-col items-center">
+            <span className="text-sm text-gray-700 dark:text-gray-400">
+              Showing{" "}
+              <span className="font-semibold text-gray-900 dark:text-white">
+                1
+              </span>{" "}
+              to{" "}
+              <span className="font-semibold text-gray-900 dark:text-white">
+                10
+              </span>{" "}
+              of{" "}
+              <span className="font-semibold text-gray-900 dark:text-white">
+                100
+              </span>{" "}
+              Entries
+            </span>
+            <div className="inline-flex mt-2 xs:mt-0">
+              <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Prev
+              </button>
+              <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Next
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-sm text-gray-700 dark:text-gray-400">
+              Showing{" "}
+              <span className="font-semibold text-gray-900 dark:text-white">
+                1
+              </span>{" "}
+              to{" "}
+              <span className="font-semibold text-gray-900 dark:text-white">
+                10
+              </span>{" "}
+              of{" "}
+              <span className="font-semibold text-gray-900 dark:text-white">
+                100
+              </span>{" "}
+              Entries
+            </span>
+            <div className="inline-flex mt-2 xs:mt-0">
+              <button className="flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Prev
+              </button>
+              <button className="flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Next
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
 interface TableProps {
-  dataSource: any[];
-  columnsName: string[];
+  dataSource?: [string, string][][];
+  columnsName?: string[];
+  totalCount?: number;
 }
